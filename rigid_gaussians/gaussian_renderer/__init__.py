cf8f33d586832420ca779db6cbf19b1bf47c5a65
Jakob Nazarenus <jna@informatik.uni-kiel.de>
2024-09-18 15:46:59 +0200

    Cleanup

--- rigid_gaussians/gaussian_renderer/__init__.py
+++ rigid_gaussians/gaussian_renderer/__init__.py
@@ -13,9 +13,7 @@ import torch
 import math
 from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
 from scene.gaussian_model import GaussianModel
-from utils.sh_utils import eval_sh, RGB2SH
 from utils.graphics_utils import getWorld2View2, getProjectionMatrix
-from utils.image_utils import discrete_colors
 import numpy as np
 
 projection_mat = np.array([
@@ -87,18 +85,12 @@ def quaternion_to_rotation_matrix(q):
 
 # Compute the product of a set of quaternions with a single quaternion (w, x, y, z)
 def quaternion_multiply(q1, q2, left_multiply=True):
-    # if left_multiply:
     # Left multiplication q1 * q2
     w = q1[:, 0]*q2[0] - q1[:, 1]*q2[1] - q1[:, 2]*q2[2] - q1[:, 3]*q2[3]
     x = q1[:, 0]*q2[1] + q1[:, 1]*q2[0] + q1[:, 2]*q2[3] - q1[:, 3]*q2[2]
     y = q1[:, 0]*q2[2] - q1[:, 1]*q2[3] + q1[:, 2]*q2[0] + q1[:, 3]*q2[1]
     z = q1[:, 0]*q2[3] + q1[:, 1]*q2[2] - q1[:, 2]*q2[1] + q1[:, 3]*q2[0]
-    # else:
-    #     # Right multiplication q2 * q1
-    #     w = q2[0]*q1[:, 0] - q2[1]*q1[:, 1] - q2[2]*q1[:, 2] - q2[3]*q1[:, 3]
-    #     x = q2[0]*q1[:, 1] + q2[1]*q1[:, 0] + q2[2]*q1[:, 3] - q2[3]*q1[:, 2]
-    #     y = q2[0]*q1[:, 2] - q2[1]*q1[:, 3] + q2[2]*q1[:, 0] + q2[3]*q1[:, 1]
-    #     z = q2[0]*q1[:, 3] + q2[1]*q1[:, 2] - q2[2]*q1[:, 1] + q2[3]*q1[:, 0]
+
 
     return torch.stack([w, x, y, z], dim=1)
 
@@ -155,19 +147,6 @@ def render_occlusion(viewpoint_camera,
     means3D_all = torch.zeros_like(pc.get_xyz, dtype=pc.get_xyz.dtype, device="cuda")
     rotations_all = torch.zeros_like(pc.get_rotation, dtype=pc.get_rotation.dtype, device="cuda")
     for rigid_body_index in range(num_rigid_bodies):
-        # if overwrite_tforms is not None:
-        #     transform = overwrite_tforms[rigid_body_index-1] # Needs to be refactored
-        # else:
-        #     transform = torch.from_numpy(viewpoint_camera.Es[rigid_body_index]).float().cuda()
-        # camera_extrinsics = torch.from_numpy(viewpoint_camera.camera_extrinsics).float().cuda()
-        # if rigid_body_index == 0:
-        #     world_to_cam = camera_extrinsics @ transform
-        # else:
-        #     world_to_cam = camera_extrinsics @ transform
-
-        # R = world_to_cam[:3, :3]
-        # T = world_to_cam[:3, 3]
-
         if rigid_body_index > 0:
             if overwrite_tforms is not None:
                 transform = overwrite_tforms[rigid_body_index-1] # Needs to be refactored
@@ -210,29 +189,6 @@ def render_occlusion(viewpoint_camera,
             opacity = opacity[render_mask]
             scales = scales[render_mask] 
             shs = shs[render_mask]
-
-    # # Color all points close to the endeffector as white, the rest as black
-    # ee_dist = torch.sqrt(torch.sum((means3D - endeffector[None, :])**2, 1))
-    # is_close = ee_dist < ee_thresh
-    # shs[is_close, :, :] = 2.0
-    # shs[~is_close, :, :] = -2.0
-
-    # # Add additional gray points to the endeffector
-    # means3D_ee = torch.randn((100, 3)).float().cuda()
-    # means3D_ee /= torch.sqrt(torch.sum(means3D_ee**2, 1, keepdim=True))
-    # means3D_ee = means3D_ee * ee_thresh + endeffector[None, :]
-    # rotations_ee = torch.zeros((means3D.shape[0], 4)).float().cuda()
-    # rotations_ee[:, 0] = 1.0
-    # opacity_ee = torch.ones((means3D_ee.shape[0], 1)).float().cuda()
-    # scales_ee = torch.ones((means3D_ee.shape[0], 3)).float().cuda() * 1e-3
-    # shs_ee = torch.zeros((means3D_ee.shape[0], shs.shape[1], shs.shape[2])).float().cuda()
-    # shs_ee[:, :, :] = 2.0
-
-    # means3D = torch.cat([means3D, means3D_ee], dim=0)
-    # rotations = torch.cat([rotations, rotations_ee], dim=0)
-    # opacity = torch.cat([opacity, opacity_ee], dim=0)
-    # scales = torch.cat([scales, scales_ee], dim=0)
-    # shs = torch.cat([shs, shs_ee], dim=0)
 
     # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
     screenspace_points = torch.zeros_like(means3D, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda") + 0
@@ -409,24 +365,8 @@ def render(viewpoint_camera,
     except:
         pass
 
-       
-    # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
-    # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
     colors_precomp = None
-    # if override_color is None:
-    #     if pipe.convert_SHs_python:
-    #         shs_view = pc.get_features.transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
-    #         dir_pp = (pc.get_xyz - viewpoint_camera.camera_center.repeat(pc.get_features.shape[0], 1))
-    #         dir_pp_normalized = dir_pp/dir_pp.norm(dim=1, keepdim=True)
-    #         sh2rgb = eval_sh(pc.active_sh_degree, shs_view, dir_pp_normalized)
-    #         colors_precomp = torch.clamp_min(sh2rgb + 0.5, 0.0)
-    #     else:
-    #         if shared_gs:
-    #             shs = pc.get_features
-    #         else:
-    #             shs = torch.cat([gaussians_l[0].get_features, gaussians_l[1].get_features], dim=0)
-    # else:
-    #     colors_precomp = override_color
+
 
     # Project the coordinate origin to the camera
     origin_2d = viewpoint_camera.camera_intrinsics @ projection_mat @ viewpoint_camera.camera_extrinsics @ np.array([0, 0, 0, 1])[:, None]
